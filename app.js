57eb23603c7634bb654b54c59176b25ade5c3edc
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2026-09-22 12:45:50 -0700

    Integrated Socket.io

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 // Import express (js flask), like import module in Python
 const express = require('express');
-// Create express app
+// Create Express App
 const app = express();
-
 // Socket.io needs access to the HTTP server
 const server = require('http').Server(app);
-// Handlebars
-const exphbs = require('express-handlebars');
 
+// Socketio
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+    console.log("🔌 New user connected! 🔌");
+})
+
+// Reister Handlebars
+const exphbs = require('express-handlebars');
 // App use handlebars template
 app.engine('handlebars', exphbs.engine());
 // Look for handlebars file
 app.set('view engine', 'handlebars');
+// Establish public folder for browser to access
+app.use('/public', express.static('public'))
 
 // Render views / index.handlebars
+// Create Route '/'
 app.get('/', (req, res) => {
     res.render('index');
 });
