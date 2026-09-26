ef93d7c591cc15c67bc2b2d6872e34953e5db6af
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2026-09-26 12:30:09 -0700

    Users can join chat and send messages

--- sockets/chat.js
+++ sockets/chat.js
@@ -5,4 +5,11 @@ module.exports = (io, socket) => {
     //Send the username to all clients currently connected
     io.emit("new user", username);
   })
+
+  //Listen for new messages
+  socket.on('new message', (data) => {
+    // Send that data back to ALL clients
+    console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+    io.emit('new message', data);
+  })
 }
